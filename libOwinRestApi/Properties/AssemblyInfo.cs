e916d5b3504e2d86f6af8c2d5128b24ac73559ab
Mike Edwards <mike@sqlby.me>
2016-03-09 22:07:03 -0800

    Adding package info and assembly info.

--- libOwinRestApi/Properties/AssemblyInfo.cs
+++ libOwinRestApi/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("libOwinRestApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Simple REST Api Self Hosting Server for a .NET Application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Systems West")]
 [assembly: AssemblyProduct("libOwinRestApi")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
